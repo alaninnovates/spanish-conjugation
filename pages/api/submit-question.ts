91baff5c7d13ef54619e473ea6eacf899898ae85
Alan <alanlichen1@gmail.com>
2024-08-31 00:15:12 -0700

    Add session logging to question submit

--- pages/api/submit-question.ts
+++ pages/api/submit-question.ts
@@ -22,6 +22,13 @@ export default async function handler(
 			.status(400)
 			.json({ error: 'sessionId and answers are required' });
 	}
+	const [session] = await sql`
+	SELECT id
+	FROM sessions
+	WHERE id = ${sessionId}`;
+	if (!session) {
+		return res.status(400).json({ error: 'Invalid session id' });
+	}
 	const [dbQuestion] = await sql`
 	SELECT q.id as questionId, q.englishname, q.spanishname,
 		t.id as tenseId, t.yo, t.tu, t.el, t.nosotros, t.vosotros, t.ellos, t.tense, t.mood, t.translation
@@ -29,10 +36,6 @@ export default async function handler(
 		JOIN tenses t ON q.id = t.questionId
 		WHERE q.id = (SELECT activeQuestionId FROM sessions WHERE id = ${sessionId})
 		`;
-	await sql`
-	UPDATE sessions
-	SET activeQuestionId = NULL
-	WHERE id = ${sessionId}`;
 	const incorrectFields = [];
 	if (answers.yo !== dbQuestion.yo) {
 		incorrectFields.push({ field: 'yo', correctValue: dbQuestion.yo });
@@ -61,6 +64,17 @@ export default async function handler(
 			correctValue: dbQuestion.ellos,
 		});
 	}
+	if (incorrectFields.length === 0) {
+		await sql`
+	UPDATE sessions
+	SET activeQuestionId = NULL
+	WHERE id = ${sessionId}`;
+	}
+	console.log(incorrectFields, dbQuestion);
+
+	await sql`
+	INSERT INTO sessionlogs (sessionId, questionId, incorrectData)
+	VALUES (${sessionId}, ${dbQuestion.questionid}, ${incorrectFields})`;
 	console.log(incorrectFields);
 	res.status(200).json({
 		incorrectFields,
